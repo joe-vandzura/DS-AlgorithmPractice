0388ee5344e73cb41b5c8e35a2cb2e3cdcec3d6b
Joe Vandzura <109103359+joe-vandzura@users.noreply.github.com>
2023-07-01 16:48:11 -0500

    Time: 1 ms (91.81%), Space: 40.8 MB (67.99%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,32 +1,32 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-//         int count = 0;
-        
-//         for (int i = 0; i < stones.length(); i++) {
-//             for (int j = 0; j < jewels.length(); j++) {
-//                 if (stones.charAt(i) == jewels.charAt(j)) {
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-        
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        
-        for (int i = 0; i < jewels.length(); i++) {
-            map.put(jewels.charAt(i), 0);
-        }
+        int count = 0;
         
         for (int i = 0; i < stones.length(); i++) {
-            if (map.containsKey(stones.charAt(i))) {
-                map.replace(stones.charAt(i), map.get(stones.charAt(i)) + 1);
+            for (int j = 0; j < jewels.length(); j++) {
+                if (stones.charAt(i) == jewels.charAt(j)) {
+                    count++;
+                }
             }
         }
-        int sum = 0;
+        return count;
         
-        for (int num : map.values()) {
-            sum += num;
-        }
-        return sum;
+//         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        
+//         for (int i = 0; i < jewels.length(); i++) {
+//             map.put(jewels.charAt(i), 0);
+//         }
+        
+//         for (int i = 0; i < stones.length(); i++) {
+//             if (map.containsKey(stones.charAt(i))) {
+//                 map.replace(stones.charAt(i), map.get(stones.charAt(i)) + 1);
+//             }
+//         }
+//         int sum = 0;
+        
+//         for (int num : map.values()) {
+//             sum += num;
+//         }
+//         return sum;
     }
 }
