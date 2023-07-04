6cb131ee2c90040b920f0b8d34560fccd72541d3
Joe Vandzura <109103359+joe-vandzura@users.noreply.github.com>
2023-07-04 16:47:25 -0500

    Time: 0 ms (100.00%), Space: 40.6 MB (43.54%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
@@ -1,34 +1,38 @@
 class Solution {
     public boolean squareIsWhite(String coordinates) {
-        boolean isWhite = false;
+//         boolean isWhite = false;
         
-        HashMap<Character, Integer> letterToNum = new HashMap<Character, Integer>();
+//         HashMap<Character, Integer> letterToNum = new HashMap<Character, Integer>();
         
-        letterToNum.put('a', 0);
-        letterToNum.put('b', 1);
-        letterToNum.put('c', 2);
-        letterToNum.put('d', 3);
-        letterToNum.put('e', 4);
-        letterToNum.put('f', 5);
-        letterToNum.put('g', 6);
-        letterToNum.put('h', 7);
+//         letterToNum.put('a', 0);
+//         letterToNum.put('b', 1);
+//         letterToNum.put('c', 2);
+//         letterToNum.put('d', 3);
+//         letterToNum.put('e', 4);
+//         letterToNum.put('f', 5);
+//         letterToNum.put('g', 6);
+//         letterToNum.put('h', 7);
         
-        for (int i = 0; i < letterToNum.get(coordinates.charAt(0)); i++) {
-            if (isWhite) {
-                isWhite = false;
-            } else {
-                isWhite = true;
-            }
-        }
+//         for (int i = 0; i < letterToNum.get(coordinates.charAt(0)); i++) {
+//             if (isWhite) {
+//                 isWhite = false;
+//             } else {
+//                 isWhite = true;
+//             }
+//         }
         
-        for (int i = 1; i < coordinates.charAt(1); i++) {
-            if (isWhite) {
-                isWhite = false;
-            } else {
-                isWhite = true;
-            }
-        }
+//         for (int i = 1; i < coordinates.charAt(1); i++) {
+//             if (isWhite) {
+//                 isWhite = false;
+//             } else {
+//                 isWhite = true;
+//             }
+//         }
         
-        return isWhite;
+//         return isWhite;
+        
+        int letter = coordinates.charAt(0);
+        int num = coordinates.charAt(1);
+        return !((letter + num) % 2 == 0);
     }
 }
