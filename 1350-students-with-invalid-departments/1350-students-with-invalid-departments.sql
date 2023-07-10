8f826b2f11a66e3117017faf77508d6d1ce2fa29
Joe Vandzura <109103359+joe-vandzura@users.noreply.github.com>
2023-07-10 17:17:05 -0500

    Time: 1902 ms (48.62%), Space: 0B (100.00%) - LeetHub

--- 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
+++ 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT s.id, s.name FROM Departments d RIGHT JOIN Students s ON d.id = s.department_id WHERE d.id IS NULL;
+SELECT s.id, s.name FROM Departments d RIGHT JOIN Students s ON d.id = s.department_id WHERE d.id IS NULL
