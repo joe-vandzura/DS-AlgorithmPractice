81faeb37e03c43faff57f4965ff440e0502b0447
Joe Vandzura <109103359+joe-vandzura@users.noreply.github.com>
2023-09-03 21:44:04 -0500

    Time: 0 ms (100.00%), Space: 40.4 MB (73.72%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,14 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        // String defanged = "";
-        // for (int i = 0; i < address.length(); i++) {
-        //     if (address.charAt(i) == '.') {
-        //         defanged += "[.]";
-        //     } else {
-        //         defanged += address.charAt(i);
-        //     }
-        // }
-        // return defanged;
         return address.replace(".", "[.]");
     }
 }
