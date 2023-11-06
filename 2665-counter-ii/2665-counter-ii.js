18dbd22058eebeb2e910d44fc30da5d77c902fc7
Joe Vandzura <109103359+joe-vandzura@users.noreply.github.com>
2023-11-06 15:13:01 -0600

    Time: 60 ms (48.36%), Space: 45 MB (15.75%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+    
+    function increment() {
+        return ++currentValue;
+    }
+    
+    function decrement() {
+        return --currentValue;
+    }
+    
+    function reset() {
+        currentValue = init;
+        return currentValue;
+    }
+    
+    return {increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
